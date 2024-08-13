a97a4c0eac36f0baf7a6bc6d23cbc894f123afc8
austintyrome <austintyrome@gmail.com>
2024-08-12 19:36:43 -0600

    Added ENV

--- client/src/ApolloProvider.jsx
+++ client/src/ApolloProvider.jsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // Create an Apollo Client instance
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql', // Replace with your GraphQL server URI
+  uri: process.env.REACT_APP_URL || 'http://localhost:3001/graphql', // Replace with your GraphQL server URI
   cache: new InMemoryCache(),
 });
 
